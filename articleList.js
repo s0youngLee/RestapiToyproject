1e862cc9b86daba61a013e1b90d547133fa52807
s0youngLee <96279331+s0youngLee@users.noreply.github.com>
2022-09-21 12:37:16 +0900

    0921 progress

--- articleList.js
+++ articleList.js
@@ -1,40 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css'
 
-function Article({ data }) {
+function Article({article}) {
     return (
       <div>
-        <b>ID : </b> <span>({data.id})</span> <br/>
-        <b>Title : </b> <span>({data.title})</span> <br/>
-        <b>Category : </b> <span>({data.category_name})</span> <br/>
-        <b>Created By : </b> <span>({data.created_id})</span> <br/>
-        <b>Created At : </b> <span>({data.created_at})</span> <br/>
-        <b>Visit : </b> <span>({data.visit_cnt})</span> <br/>
-        <b>Comment : </b> <span>({data.comment_cnt})</span> <br/>
+        {/* article만 불러오면 출력이 안됨, .data 또는 .id 등 붙이면 못읽음
+            articleList에 값이 아직 없는 상태에서 출력하려고 하는 것 같음 */}
+        {/* <b>ID : </b> <span>{article.data(0).id}</span> <br/> */}
+        <b>ID : </b> <span>{article}</span> <br/>
+        <b>Title : </b> <span>{article}</span> <br/>
+        <b>Category : </b> <span>{article}</span> <br/>
+        <b>Created By : </b> <span>{article}</span> <br/>
+        <b>Created At : </b> <span>{article}</span> <br/>
+        <b>Visit : </b> <span>{article}</span> <br/>
+        <b>Comment : </b> <span>{article}</span> <br/>
       </div>
     );
-  }
+}
 
 
-function ArticleLists({props}){
-    // const articleList = props.data;
-    // const articles = data && data.map((article, index) => {
-    //     return <div key={index}>
-    //                 <span>{article[0]}</span>
-    //                 <br/>
-    //            </div>
-    // });
+function ArticleLists(props){
+    const [articleList, setArticleList] = useState({
+        data : 
+            {
+                id: 'DEFAULT',
+                title: 'DEFAULT',
+                created_id: 'DEFAULT',
+                created_at: 'DEFAULT',
+                category_name: 'DEFAULT',
+                visit_cnt: 'DEFAULT',
+                comment_cnt: 'DEFAULT'
+            }
+    });
+    
+    useEffect(()=> {
+        // let resData = null;
+        // async function fetchData() {
+        //     resData = await props.ArticleLists;
+        // }
+        // fetchData();
+        setArticleList(props.ArticleLists);
+        console.log(articleList);
+    },[props.ArticleLists, articleList])
 
-
+    
     return (
         <div>
             Board Data(Text → JSON) :
-            <div>
-                {props.data.map(article => (
-                    <Article article={article} /> 
-                ))}
-            </div>
-            {/* <div id="article"> {articles} </div> */}
+            <Article list={articleList}/>
         </div>
     )
 }
