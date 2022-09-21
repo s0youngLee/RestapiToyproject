c2423ab5ee3020fbcbc606a3a9464ba24eeae6ee
s0youngLee <96279331+s0youngLee@users.noreply.github.com>
2022-09-21 13:20:20 +0900

    0921 progress

--- articleList.js
+++ articleList.js
@@ -1,24 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './App.css'
 
-function Article({article}) {
-    return (
-      <div>
-        {/* article만 불러오면 출력이 안됨, .data 또는 .id 등 붙이면 못읽음
-            articleList에 값이 아직 없는 상태에서 출력하려고 하는 것 같음 */}
-        {/* <b>ID : </b> <span>{article.data(0).id}</span> <br/> */}
-        <b>ID : </b> <span>{article}</span> <br/>
-        <b>Title : </b> <span>{article}</span> <br/>
-        <b>Category : </b> <span>{article}</span> <br/>
-        <b>Created By : </b> <span>{article}</span> <br/>
-        <b>Created At : </b> <span>{article}</span> <br/>
-        <b>Visit : </b> <span>{article}</span> <br/>
-        <b>Comment : </b> <span>{article}</span> <br/>
-      </div>
-    );
-}
-
-
 function ArticleLists(props){
     const [articleList, setArticleList] = useState({
         data : 
@@ -32,24 +14,38 @@ function ArticleLists(props){
                 comment_cnt: 'DEFAULT'
             }
     });
+
     
     useEffect(()=> {
-        // let resData = null;
-        // async function fetchData() {
-        //     resData = await props.ArticleLists;
-        // }
-        // fetchData();
         setArticleList(props.ArticleLists);
         console.log(articleList);
+        console.log(articleList.data[2]);
     },[props.ArticleLists, articleList])
 
     
     return (
         <div>
             Board Data(Text → JSON) :
-            <Article list={articleList}/>
+            <Article list={articleList.data}/>
         </div>
     )
+}
+
+function Article({data}) {
+    return (
+      <ul id="ulNone" key={data}>
+        {/* article만 불러오면 출력이 안됨, .data 또는 .id 등 붙이면 못읽음
+            articleList에 값이 아직 없는 상태에서 출력하려고 하는 것 같음 */}
+        {/* <li><b>ID : </b> <span key={data[0].id}>{data[0].id}</span> <br/></li> */}
+        <li><b>ID : </b> <span key={data}>{data}</span> <br/></li>
+        <li><b>Title : </b> <span>{data}</span> <br/></li>
+        <li><b>Category : </b> <span>{data}</span> <br/></li>
+        <li><b>Created By : </b> <span>{data}</span> <br/></li>
+        <li><b>Created At : </b> <span>{data}</span> <br/></li>
+        <li><b>Visit : </b> <span>{data}</span> <br/></li>
+        <li><b>Comment : </b> <span>{data}</span> <br/></li>
+      </ul>
+    );
 }
 
 export default ArticleLists;
