4dbeaf5152a6a0e46401974e340fdecbb77cca92
s0youngLee <96279331+s0youngLee@users.noreply.github.com>
2022-09-21 12:36:50 +0900

    0921 progress

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ import {CookiesProvider} from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+//   <React.StrictMode>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </React.StrictMode>
+//   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
