0cb59aec1c1a060a4f9e27e1b1497ce320010ecf
Github <Github>
2022-11-04 15:41:54 +0900

    1104 Progress

--- src/main/front-end/app_board/src/Article/ArticleEdit.js
+++ src/main/front-end/app_board/src/Article/ArticleEdit.js
@@ -15,6 +15,7 @@ function ArticleEditForm({user, articleDetail, handleClose}){
     const inputFile = document.getElementsByName("upfile");
     const fileName = useMemo(() => {return new Array("Selected Files")},[]);
     const [files, setFiles] = useState({data : {}});
+    const [visible, setVisible] = useState(false);
 
     const editTitle = useCallback(e => {
         setTitle(e.target.value);
@@ -30,14 +31,60 @@ function ArticleEditForm({user, articleDetail, handleClose}){
     };
 
     const uploadFile = useCallback((e) => {
-        fileName.length = 0;
-        if(!_.isEmpty(inputFile)){
-            setFiles(inputFile[0].files);
-            for(let i = 0; i < inputFile[0].files.length ; i++){
-                fileName.push(inputFile[0].files[i].name);
+        const previewImg = document.getElementById('preview-img');
+        const previewFile = document.getElementById('preview-file');
+        const child = Array.from(document.getElementsByName('preview'));
+        const brs = Array.from(document.getElementsByName('br'));
+        
+        if(!_.isEmpty(inputFile[0].files)){
+            setVisible(false);        
+            fileName.length = 0;
+            if(!_.isEmpty(brs)){
+                brs.forEach(br => {
+                    previewFile.removeChild(br);
+                })
             }
+            if(!_.isEmpty(child)){
+                child.forEach(files => {
+                    if(_.isEqual(files.nodeName, "IMG")){
+                        previewImg.removeChild(files);
+                    }else if(_.isEqual(files.nodeName, "SPAN")){
+                        previewFile.removeChild(files);
+                    }
+                    URL.revokeObjectURL(files.src);
+                })
+            }
+
+            setFiles(inputFile[0].files);
+            Array.from(inputFile[0].files).forEach(file => {
+                fileName.push(" " + file.name);
+                setVisible(true);
+                const fileType = file.type.split("/")[0];
+                
+                if(_.isEqual(fileType, "image")){
+                    const image = document.createElement('img');
+                    image.setAttribute("name", "preview");
+                    image.className = "image-preview";
+                    image.setAttribute("alt", file.name);
+                    image.style = visible ? {display: "table-cell"} : {display: "none"};
+                    image.src = URL.createObjectURL(file);
+                    image.title = file.name;
+                    previewImg.appendChild(image);
+                }else if(_.isEqual(fileType, "application")){
+                    const doc = document.createElement('span');
+                    const br = document.createElement('br');
+                    br.setAttribute("name", "br");
+                    doc.setAttribute("name", "preview");
+                    doc.className = "application-preview";
+                    doc.style = visible ? {display: "table-cell"} : {display: "none"};
+                    doc.textContent = file.name;
+                    previewFile.appendChild(doc);
+                    previewFile.appendChild(br);
+                }
+            })
         }
-    }, [inputFile, fileName]);
+        
+    }, [inputFile, fileName, visible]);
     
     for(let i = 0;i < files.length; i++){
         formData.append("file", files[i]);
@@ -72,7 +119,7 @@ function ArticleEditForm({user, articleDetail, handleClose}){
             <>
                 <b style={{fontSize: "25px", textAlign: "left"}}>Edit article</b><hr/>
                 <form onSubmit={editArticle}>
-                    <div className="div-box" style={{textAlign: "left", marginTop: "10px"}}>
+                    <div className="div-box" style={{textAlign: "left", marginTop: "10px", marginBottom: "30px"}}>
                         <b style={{fontSize: "20px"}}>User ID : {user?.nick_name} </b><br/>
                         <b style={{fontSize: "17px"}}> Category : </b>
                         <select onChange={handleSelect} value={selected}>
@@ -98,11 +145,15 @@ function ArticleEditForm({user, articleDetail, handleClose}){
                     <b style={{fontSize: "25px", marginLeft: "10px"}}> File List </b>
                     <Files files={articleDetail?.files} user={user} createdId={articleDetail?.id}/>
                 </div>
-                {/* <div>
-                    {articleDetail?.files.map((file, index) => {
-                        <img key={index} src={file.save_name} alt="preview-img" />
-                    })}
-                </div> */}
+                <div id='preview-zone'>
+                    <b style={{fontSize: "30px"}}> Upload File Preview </b><br/>
+                    <div id='preview-img' style={visible ? {} : {display: "none"}}>
+                        <b style={{fontSize: "20px"}}> &lt; Image &gt; </b><br/>
+                    </div>
+                    <div id='preview-file' style={visible ? {} : {display: "none"}}>
+                        <b style={{fontSize: "20px"}}> &lt; File &gt; </b><br/>
+                    </div>
+                </div>
             </>
         )
     }
